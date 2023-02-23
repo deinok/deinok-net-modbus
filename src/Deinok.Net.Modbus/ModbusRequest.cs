8d3e4d6af3f285d831173cce44d79fdd435a2889
Raul Hidalgo Caballero <deinok@deinok.com>
2023-02-23 21:14:01 +0100

    ModbusRequest (#5)

--- src/Deinok.Net.Modbus/ModbusRequest.cs
+++ src/Deinok.Net.Modbus/ModbusRequest.cs
@@ -1,8 +1,17 @@
-﻿namespace Deinok.Net.Modbus
+﻿using System.Collections.Immutable;
+
+namespace Deinok.Net.Modbus
 {
 
     public record ModbusRequest
     {
+
+        public required byte Address { get; init; }
+
+        public required ImmutableArray<byte> Data { get; init; }
+
+        public required byte Function { get; init; }
+
     }
 
 }
