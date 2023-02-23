ec63b7149d7c439938fa3ed3bceccd128e6898ce
Raul Hidalgo Caballero <deinok@deinok.com>
2023-02-23 20:34:34 +0100

    Add Base clases

---
+++ src/Deinok.Net.Modbus.Client/ModbusMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Deinok.Net.Modbus.Client
+{
+
+    public abstract class ModbusMessageHandler : IAsyncDisposable, IDisposable
+    {
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
