0d6c265415581bf776627a8f7bf3cd163b301ee4
Raul Hidalgo Caballero <deinok@deinok.com>
2023-02-24 19:45:50 +0100

    fix-bug (#11)

--- src/Deinok.Net.Modbus.Client/ModbusClientHandler.cs
+++ src/Deinok.Net.Modbus.Client/ModbusClientHandler.cs
@@ -1,8 +1,23 @@
-﻿namespace Deinok.Net.Modbus.Client
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Deinok.Net.Modbus.Client
 {
 
     public class ModbusClientHandler : ModbusMessageHandler
     {
+
+        public override ModbusMessage Send(ModbusMessage request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override async Task<ModbusMessage> SendAsync(ModbusMessage request, CancellationToken cancellationToken = default)
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+            throw new System.NotImplementedException();
+        }
+
     }
 
 }
