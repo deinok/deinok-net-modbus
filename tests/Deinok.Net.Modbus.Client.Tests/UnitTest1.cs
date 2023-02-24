f0e84a211d8692fe31c767b4753cb6fea4391f78
Raul Hidalgo Caballero <deinok@deinok.com>
2023-02-24 19:38:49 +0100

    Test Infrastructure (#10)

---
+++ tests/Deinok.Net.Modbus.Client.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace Deinok.Net.Modbus.Client.Tests
+{
+
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+    }
+
+}
---
+++ tests/Deinok.Net.Modbus.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace Deinok.Net.Modbus.Tests
+{
+
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+    }
+
+}
