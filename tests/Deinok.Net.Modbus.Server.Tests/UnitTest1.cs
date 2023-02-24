123a45eeaada9a0b3b0e41eca3863eddd8ad5243
Raul Hidalgo Caballero <deinok@deinok.com>
2023-02-24 19:37:52 +0100

    .

---
+++ tests/Deinok.Net.Modbus.Server.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace Deinok.Net.Modbus.Server.Tests
+{
+
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+    }
+
+}
