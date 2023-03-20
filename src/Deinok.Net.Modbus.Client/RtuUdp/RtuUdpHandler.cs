5dd68d7bca9daff59ab83864add3ebbcbab2afad
Raul Hidalgo Caballero <deinok@deinok.com>
2023-03-20 20:48:00 +0100

    .

---
+++ src/Deinok.Net.Modbus.Client/RtuUdp/RtuUdpHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Deinok.Net.Modbus.Client.RtuUdp
+{
+
+    public class RtuUdpHandler : ModbusMessageHandler
+    {
+
+        private readonly UdpClient udpClient;
+
+        public RtuUdpHandler(UdpClient udpClient)
+        {
+            this.udpClient = udpClient;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.udpClient.Dispose();
+            }
+        }
+
+        public override ModbusMessage Send(ModbusMessage request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task<ModbusMessage> SendAsync(ModbusMessage request, CancellationToken cancellationToken = default)
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
