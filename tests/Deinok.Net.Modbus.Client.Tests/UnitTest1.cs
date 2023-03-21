0e83fc72789b1fc99e7f9e8ba0461a988e7fafc1
Raul Hidalgo Caballero <deinok@deinok.com>
2023-03-21 21:34:29 +0100

    .

--- tests/Deinok.Net.Modbus.Client.Tests/UnitTest1.cs
+++ tests/Deinok.Net.Modbus.Client.Tests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using Deinok.Net.Modbus.Client.Tcp;
+using System.Collections.Immutable;
+using System.Net.Sockets;
 using Xunit;
 
 namespace Deinok.Net.Modbus.Client.Tests
@@ -6,10 +9,18 @@ namespace Deinok.Net.Modbus.Client.Tests
     public class UnitTest1
     {
 
-        [Fact]
+        [Fact(Skip = "Skip")]
         public void Test1()
         {
-
+            var tcpClient = new TcpClient("localhost", 502);
+            var tcpHandler = new TcpHandler(tcpClient);
+            var modbusMessageRequest = new ModbusMessage
+            {
+                Address = 1,
+                Data = new byte[] { 0x00, 0x00, 0x00, 0x0A }.ToImmutableArray(),
+                Function = ModbusFunction.ReadMultipleHoldingRegisters,
+            };
+            var modbusMessageResponse = tcpHandler.Send(modbusMessageRequest);
         }
 
     }
