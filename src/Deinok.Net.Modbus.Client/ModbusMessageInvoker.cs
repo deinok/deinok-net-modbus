f6c437c8e55998e63753539f86b60cf2ca32cc76
Raul Hidalgo Caballero <deinok@deinok.com>
2023-04-03 20:19:35 +0200

    ModbusMessageInvoker (#22)

--- src/Deinok.Net.Modbus.Client/ModbusMessageInvoker.cs
+++ src/Deinok.Net.Modbus.Client/ModbusMessageInvoker.cs
@@ -1,21 +1,44 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Deinok.Net.Modbus.Client
 {
 
-    public class ModbusMessageInvoker : IAsyncDisposable, IDisposable
+    public class ModbusMessageInvoker : IDisposable
     {
+
+        private readonly ModbusMessageHandler modbusMessageHandler;
+        private readonly bool disposeHandler;
+
+        public ModbusMessageInvoker(ModbusMessageHandler modbusMessageHandler, bool disposeHandler = true)
+        {
+            this.modbusMessageHandler = modbusMessageHandler;
+            this.disposeHandler = disposeHandler;
+        }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
-        public async ValueTask DisposeAsync()
+        protected virtual void Dispose(bool disposing)
         {
-            await Task.CompletedTask.ConfigureAwait(false);
-            throw new NotImplementedException();
+            if (disposing && this.disposeHandler)
+            {
+                this.modbusMessageHandler.Dispose();
+            }
+        }
+
+        public ModbusMessage Send(ModbusMessage modbusMessage)
+        {
+            return this.modbusMessageHandler.Send(modbusMessage);
+        }
+
+        public async Task<ModbusMessage> SendAsync(ModbusMessage modbusMessage, CancellationToken cancellationToken = default)
+        {
+            return await this.modbusMessageHandler.SendAsync(modbusMessage, cancellationToken).ConfigureAwait(false);
         }
 
     }
